81fabfeb82d48faf9b2a9d50f5f5bdcb3598efbd
CosimoSans <164258688+CosimoSans@users.noreply.github.com>
2024-05-28 13:20:32 +0200

    Update Data_cleaning_layoffs.sql

--- SQL/Data_Cleaning/Data_cleaning_layoffs.sql
+++ SQL/Data_Cleaning/Data_cleaning_layoffs.sql
@@ -5,12 +5,17 @@ use world_layoffs
 SELECT *
 FROM layoffs
 
+-- Case Studi objectives:
+	
 -- 1. Remove Duplicates
 -- 2. Standardize the Data
 -- 3. NULL or blank values
 -- 4. Remove unnecessary columns
 
--- create a new table
+-- SQL skills involved: string functions, joins, CTE's, date manipulation, views, aggregate functions, string manipulation
+
+-- first of all: let's create a new table in order to modify and clean the data without changing the original table
+
 CREATE TABLE layoffs_staging
 LIKE layoffs;
 
@@ -26,14 +31,14 @@ FROM layoffs;
 SELECT *
 FROM layoffs;
 
--- assegnare un numero a tutte le righe uniche
+-- a method i use to remove duplicates is to assign a number to unique rows, so if any row is duplicated, it will have a different number
 
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
 FROM layoffs_staging;
 
--- creare una CTE per identificare quelli che hanno valore 2 o maggiore
+-- Now i want to search for all rows that have 'row_num' >= 2. To do this, i need to use a CTE.
 
 WITH duplicate_cte AS
 (SELECT *,
@@ -45,14 +50,8 @@ SELECT *
 FROM duplicate_cte
 WHERE row_num > 1;
 
--- facciamo un controllo a campione veloce, per vedere se effettivamente ha funzionato, e sono effettivamente duplicati
-
-SELECT *
-FROM layoffs_staging
-WHERE company = 'Oda'
-
--- abbiamo notato che non era del tutto un duplicato quindi tocca cambiare la partition. Mettiamo ogni singola colonna.
--- ora basterebbe aggiugnere alla CTE DELETE per cancellare quelli con 2, ma su mYSQL non è possibile farlo per le CTE's
+-- let's do a spot check to see if they are indeed duplicates
+-- now i should delete them, but in mysql i can't delete from CTE
 
 WITH duplicate_cte AS
 (SELECT *,
@@ -64,8 +63,8 @@ DELETE
 FROM duplicate_cte
 WHERE row_num > 1;
 
--- da errore
--- quindi per poter rimuovere questi dati devo creare una nuova tabella e cancellarli da li
+-- error
+-- so i need to create another table and delete from there, using the create table menu in wich i manually insert the new table staging2
 
 CREATE TABLE `layoffs_staging2` (
   `company` text,
@@ -83,11 +82,15 @@ CREATE TABLE `layoffs_staging2` (
 SELECT *
 FROM layoffs_staging2
 
+-- insert data again in staging2
+	
 INSERT INTO layoffs_staging2
 SELECT *,
 ROW_NUMBER() OVER(
 PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
 FROM layoffs_staging;
+
+-- delete duplicates
 
 DELETE
 FROM layoffs_staging2
@@ -96,8 +99,10 @@ WHERE row_num > 1;
 SELECT *
 FROM layoffs_staging2;
 
+-- Duplicates removed
 -- STANDARDIZING DATA
 
+-- Remove unnecessary spaces and update the tables
 SELECT company, TRIM(company)
 FROM layoffs_staging2
 
@@ -106,6 +111,9 @@ SET company = TRIM(company)
  
 SELECT *
 FROM layoffs_staging2;
+
+
+-- then we need to standardize the 'industry' column
 
 SELECT Distinct industry
 FROM layoffs_staging2
@@ -119,7 +127,8 @@ UPDATE layoffs_staging2
 SET industry = 'Crypto'
 WHERE industry LIKE 'Crypto%'
 
--- update country
+-- update country (i found a '.' after 'United States')
+	
 SELECT DISTINCT country
 FROM layoffs_staging2
 Order by 1
@@ -131,6 +140,8 @@ Order by 1
 UPDATE layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country)
 WHERE country LIKE 'United States%'
+
+-- standardize date
 
 SELECT `date`,
 STR_TO_DATE(`date`, '%m/%d/%Y')
@@ -147,36 +158,25 @@ Modify Column `date` DATE;
 
 -- blank and NULL
 
-SELECT * 
-FROM layoffs_staging2
-WHERE total_laid_off is NULL
-and percentage_laid_off is NULL;
-
-DELETE 
-FROM layoffs_staging2
-WHERE total_laid_off is NULL
-and percentage_laid_off is NULL; -- nel caso in cui volessi cancellarlo
-
--- non abbiamo modo di inserire dei dati in queste colonne, proviamo in altre colonne
-
 SELECT *
 FROM layoffs_staging2
 where industry is NULL
 OR industry ='';
 
--- ad esempio in industry abbiamo 3 vuoti e un NULL, vediamo se le aziende che non hanno una industry, ce l'hanno in altre righe (si)
+-- let's compare the companies that have 'blank' or NULL in industry with the industry of the same company but on another row
 
 SELECT *
 FROM layoffs_staging2
 WHERE company = 'airbnb';
 
--- si, quindi per airbnb in industry dobbiamo mettere travel ecc. per farlo, però, dobbiamo prima sostituire tutti i blank con NULL altrimenti da errore
+-- i found that airbnb has 'travel' in industry, so yeah, i can replace every blank cell with the industry of another row (for the same company)
+-- but for doing that, i have to replace every blank cell with NULL
 
 UPDATE layoffs_staging2
 SET industry = NULL
 WHERE industry = '';
 
--- adesso sono tutti NULL. adesso creaiamo due tabelle, una in cui l'industry è null e una in cui è uguale a quello che dovrebbe essere
+-- now i have only NULL. let's create two tables: one with industry = Null and one with the right industry
 
 SELECT *
 FROM layoffs_staging2 t1
@@ -186,7 +186,6 @@ JOIN layoffs_staging2 t2
 WHERE (t1.industry is null OR t1.industry = '')
 AND t2.industry is NOT null;
 
--- mettiamo a confronto le due industry
 
 SELECT t1.industry,t2.industry
 FROM layoffs_staging2 t1
@@ -196,7 +195,7 @@ JOIN layoffs_staging2 t2
 WHERE t1.industry is null
 AND t2.industry is NOT null;
 
--- ora sostituiamo i NULL con l'industry corretta
+-- now i can replace the NULL with the industry
 
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2 t2
@@ -206,7 +205,7 @@ WHERE t1.industry is NULL
 AND t2.industry is NOT NULL;
  
 
--- eliminare la colonna usata per le righe e che non ci serve piu row_num
+-- Delete unnecessary columns
 
 SELECT *
 FROM layoffs_staging2;
@@ -216,14 +215,6 @@ DROP column row_num;
 
 -- CLEAN
 
-Select *
-FROM layoffs_staging2
-where stage is NULL;
-
-
-Select *
-from layoffs_staging2
-where company = 'Spreetail'
 
 
 
